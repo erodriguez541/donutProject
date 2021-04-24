3ebee2f83686b900f28245fe7921480d15ab8ef5
Christopher <sayegh.topher@gmail.com>
2021-04-24 13:30:47 -0500

    google api is functioning

--- Assets/script.js
+++ Assets/script.js
@@ -1,33 +1,43 @@
-const apiData = {
-    url: '',
-    type: '',
-    id: ,
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+let map;
+let service;
+let infowindow;
+
+
+function initMap() {
+  const sydney = new google.maps.LatLng(-33.867, 151.195);
+  infowindow = new google.maps.InfoWindow();
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: sydney,
+    zoom: 15,
+  });
+  const request = {
+    query: "Museum of Contemporary Art Australia",
+    fields: ["name", "geometry"],
+  };
+  service = new google.maps.places.PlacesService(map);
+  service.findPlaceFromQuery(request, (results, status) => {
+    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+      for (let i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+      map.setCenter(results[0].geometry.location);
+    }
+  });
 }
 
-const {url, type, id} = apiData
+function createMarker(place) {
+  if (!place.geometry || !place.geometry.location) return;
+  const marker = new google.maps.Marker({
+    map,
+    position: place.geometry.location,
+  });
+  google.maps.event.addListener(marker, "click", () => {
+    infowindow.setContent(place.name || "");
+    infowindow.open(map);
+  });
+}
 
-const apiData = `${url}${type}/${id}`
-
-fetch(apiUrl)
-    .then( (data) => {
-        if(data.ok){
-            return data.json()
-        }
-        throw new Error('Response not ok.'); 
-    })
-    .then(  => generateHtml())
-    .catch( error => console.error('Error:', error))
-
-
-const generateHtml = (data) => {
-    console.log(data)
-    const html = `
-        <div class="name">${data.name}</div>
-        <img src=${data.sprites.front_default}>
-        <div class="details">
-            <span>Height: ${data.height}</span>
-            <span>Weight: ${data.weight}</span>
-        </div>
-    `
-    const donutDiv = document.querySelector('')
-    donutDiv.innerHTML = html
+initMap();
